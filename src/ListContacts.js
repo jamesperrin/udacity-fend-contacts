fac125bd463a3af5710ba9b3ec87d0e4801ce874
James Perrin <countrydawgg@gmail.com>
2018-08-27 15:14:29 -0700

    Convert Component Class to Functional Component.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react'
 
-class ListContacts extends Component { 
-    render() { 
-        const contacts = this.props.contacts;
+// Short version - Stateless Functional Component
+const ListContacts = (props) => (
+    <ol className='cotact-list'>
+        {props.contacts.map(contact => (
+            <li key={contact.id} className='contact-list-item'>
+                <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}} />
+                <div className='contact-details'>
+                    <p>{contact.name}</p>
+                    <p>{contact.email}</p>
+                </div>
+                <button className='contact-remove'>
+                    Remove
+                </button>
+            </li>
+        ))}
+    </ol>
+)
+ 
+//Stateless Functional Component
+// function ListContacts(props) {
+//     const contacts = props.contacts;
+//
+//     return (
+//         <ol className='cotact-list'>
+//             {contacts.map(contact => (
+//                 <li key={contact.id} className='contact-list-item'>
+//                     <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}} />
+//                     <div className='contact-details'>
+//                         <p>{contact.name}</p>
+//                         <p>{contact.email}</p>
+//                     </div>
+//                     <button className='contact-remove'>
+//                         Remove
+//                     </button>
+//                 </li>
+//             ))}
+//         </ol>
+//     )
+//  }
 
-        return (
-            <ol className='cotact-list'>
-                {contacts.map(contact => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div className='contact-avatar' style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }} />
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.email}</p>
-                        </div>
-                        <button className='contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
-}
+
+// class ListContacts extends Component { 
+//     render() { 
+//         const contacts = this.props.contacts;
+
+//         return (
+//             <ol className='cotact-list'>
+//                 {contacts.map(contact => (
+//                     <li key={contact.id} className='contact-list-item'>
+//                         <div className='contact-avatar' style={{
+//                             backgroundImage: `url(${contact.avatarURL})`
+//                         }} />
+//                         <div className='contact-details'>
+//                             <p>{contact.name}</p>
+//                             <p>{contact.email}</p>
+//                         </div>
+//                         <button className='contact-remove'>
+//                             Remove
+//                         </button>
+//                     </li>
+//                 ))}
+//             </ol>
+//         )
+//     }
+// }
 
 export default ListContacts
