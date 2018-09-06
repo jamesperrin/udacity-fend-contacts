cb20a3a460b3fb37ad00d5e6bb1b5ee9bfd7611f
James Perrin <countrydawgg@gmail.com>
2018-09-05 21:09:08 -0700

    Add CreateContact form with form serialization capturing.

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
                     />
                 )}/>
 
-                <Route path='/create' component={CreateContact}/>
+                <Route path='/create' component={CreateContact} />
         </div>
         )
     }
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component { 
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const formValues = serializeForm(e.target, { hash: true });
+        
+        if (this.props.onCreateContact) {
+            this.props.onCreateContact(formValues);
+        }
+    }
+
     render() { 
         return (
             <div>
                 <Link to='/' className='close-create-contact' title='Close'>Close</Link>
-                <form className='create-contact-form'>
+                <form onSubmit={this.handleSubmit} className='create-contact-form'>
                     <ImageInput className='create-contact-avatar-input' name='avatarURL' maxHeight={64} />
                     <div className='create-contact-details'>
                         <input type='text' name='name' placeholder='Name' />
